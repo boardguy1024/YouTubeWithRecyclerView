f63d09479074a8569836a7f8447504c763429165
park kyung suk <boardguy1024@gmail.com>
2018-09-10 14:30:46 +0900

    Picasso libsでイメージを取得

--- app/src/main/java/com/example/parkkyungsuk/youtubewithrecyclerview/MainAdapter.kt
+++ app/src/main/java/com/example/parkkyungsuk/youtubewithrecyclerview/MainAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.video_row.view.*
 
 class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolder>() {
@@ -26,6 +27,13 @@ class MainAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolde
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         val video = homeFeed.videos.get(position)
         holder.view.textView_video_title.text = video.name
+        holder.view.textView_channel_name.text = video.channel.name
+
+        val thumnailImageView = holder.view.imageView_thumnail
+        Picasso.get().load(video.imageUrl).into(thumnailImageView)
+
+        val channelImageView = holder.view.imageView_icon
+        Picasso.get().load(video.channel.profileImageUrl).into(channelImageView)
     }
 }
 
